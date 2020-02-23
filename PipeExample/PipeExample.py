3fb39f1418d0f401869d538fbcf530afc519cccc
Conner Weatherston <connerweatherston@hotmail.co.uk>
2020-02-23 19:05:14 +0000

    update python script to take in command line arguments

--- PipeExample/PipeExample.py
+++ PipeExample/PipeExample.py
@@ -1,7 +1,25 @@
 import subprocess as sp
 from subprocess import Popen 
 import os
-
+import sys
+import getopt
+ 
+# Read command line args
+myopts, args = getopt.getopt(sys.argv[1:],"p:")
+ 
+exampleArg=''
+###############################
+# o == option
+# a == argument passed to the o
+###############################
+for o, a in myopts:
+    if o == '-p':
+        exampleArg = a
+    else:
+        print("Usage: %s -p exampleArg" % sys.argv[0])
+ 
+# Display input and output file name passed as the args
+print ("exampleArg: %s" % (exampleArg) )
 exe = "../Debug/ApplicationToPipeFrom.exe"
 runningApp = Popen(exe,stdout = sp.PIPE, universal_newlines = True)
 capturedArguments = runningApp.communicate()[0]
@@ -9,4 +27,4 @@ print("Command line from app 1: " + capturedArguments)
 
 print("App 2:")
 exe = "../Debug/ApplicationToPipeToo.exe"
-runningApp = Popen(exe + " " + capturedArguments)
+runningApp = Popen([exe, capturedArguments, "-p " + exampleArg])
